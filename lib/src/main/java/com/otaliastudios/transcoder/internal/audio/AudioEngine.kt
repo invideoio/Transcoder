56462c79c7194ee044f263556a8137ae709f2812
Dhruv <dhruv2295@gmail.com>
2022-04-13 20:13:50 +0530

    wip

--- lib/src/main/java/com/otaliastudios/transcoder/internal/audio/AudioEngine.kt
+++ lib/src/main/java/com/otaliastudios/transcoder/internal/audio/AudioEngine.kt
@@ -17,6 +17,9 @@ import com.otaliastudios.transcoder.internal.pipeline.State
 import com.otaliastudios.transcoder.internal.utils.Logger
 import com.otaliastudios.transcoder.resample.AudioResampler
 import com.otaliastudios.transcoder.stretch.AudioStretcher
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.math.ceil
 import kotlin.math.floor
@@ -53,12 +56,14 @@ class AudioEngine(
         this.rawFormat = rawFormat
         remixer = AudioRemixer[rawFormat.channels, targetFormat.channels]
         chunks = ChunkQueue(rawFormat.sampleRate, rawFormat.channels)
+        createStream(rawFormat.sampleRate, targetFormat.sampleRate, targetFormat.channels)
     }
 
     override fun enqueueEos(data: DecoderData) {
         log.i("enqueueEos()")
         data.release(false)
         chunks.enqueueEos()
+        destroyStream()
     }
 
     override fun enqueue(data: DecoderData) {
@@ -68,6 +73,9 @@ class AudioEngine(
             data.release(false)
         }
     }
+    val outputFloatBuffer = ByteBuffer.allocateDirect(32096 * Float.SIZE_BYTES)
+        .order(ByteOrder.LITTLE_ENDIAN)
+        .asFloatBuffer()
 
     override fun drain(): State<EncoderData> {
         if (chunks.isEmpty()) {
@@ -78,11 +86,11 @@ class AudioEngine(
             log.i("drain(): no next buffer, waiting...")
             State.Wait
         }
-        val outBuffer = outBytes.asShortBuffer()
+        val outShortBuffer = outBytes.asShortBuffer()
         return chunks.drain(
             eos = State.Eos(EncoderData(outBytes, outId, 0))
         ) { inBuffer, timeUs, stretch ->
-            val outSize = outBuffer.remaining()
+            val outSize = outShortBuffer.remaining()
             val inSize = inBuffer.remaining()
 
             // Compute the desired output size based on all steps that we'll go through
@@ -105,27 +113,55 @@ class AudioEngine(
 
             // Remix
             val remixSize = remixer.getRemixedSize(stretchSize.toInt())
-            val remixBuffer = buffers.acquire("remix", remixSize)
-            remixer.remix(stretchBuffer, remixBuffer)
-            remixBuffer.flip()
+            val remixShortBuffer = buffers.acquire("remix", remixSize)
+            remixer.remix(stretchBuffer, remixShortBuffer)
+            remixShortBuffer.flip()
 
             // Resample
-            resampler.resample(
-                remixBuffer,
-                rawFormat.sampleRate,
-                outBuffer,
-                targetFormat.sampleRate,
-                targetFormat.channels
-            )
-            outBuffer.flip()
+//            resampler.resample(
+//                remixShortBuffer,
+//                rawFormat.sampleRate,
+//                outShortBuffer,
+//                targetFormat.sampleRate,
+//                targetFormat.channels
+//            )
+            log.v("Drain to Resampler: ts: $timeUs  limit: ${remixShortBuffer.limit()} capacity: ${remixShortBuffer.capacity()}")
 
-            // Adjust position and dispatch.
+            val size = remixShortBuffer.limit()
+            val input = ByteBuffer.allocateDirect(size * Float.SIZE_BYTES)
+                .order(ByteOrder.LITTLE_ENDIAN)
+                .asFloatBuffer()
+            for(i in 0 until size) {
+                input.put(remixShortBuffer[i].toFloat())
+            }
+
+            outputFloatBuffer.position(0)
+
+            val dataRead = getSampledByteBuffer(targetFormat.channels,input, outputFloatBuffer, remixShortBuffer.limit() / targetFormat.channels)
+
+            outputFloatBuffer.limit(dataRead)
+
+
+//            val outShortBuffer1 = outBytes.asShortBuffer()
+            for(i in 0 until outputFloatBuffer.limit()) {
+                outShortBuffer.put(outputFloatBuffer[i].toInt().toShort())
+            }
+            outShortBuffer.flip()
+
             outBytes.clear()
-            outBytes.limit(outBuffer.limit() * BYTES_PER_SHORT)
-            outBytes.position(outBuffer.position() * BYTES_PER_SHORT)
+            // Adjust position and dispatch.
+            outBytes.limit(outShortBuffer.limit() * Short.SIZE_BYTES)
+            outBytes.position(outShortBuffer.position() * Float.SIZE_BYTES)
 
-            log.v("Drain: ts: $timeUs  size: ${outBuffer.limit()}")
             State.Ok(EncoderData(outBytes, outId, timeUs))
         }
     }
+
+    // Creates and starts Oboe stream to play audio
+    private external fun createStream(inputSampleRate: Int, outputSampleRate: Int, channelCount: Int) : Int
+
+    // Closes and destroys Oboe stream when app goes out of focus
+    private external fun destroyStream()
+
+    private external fun getSampledByteBuffer(channelCount: Int, inputBuffers: FloatBuffer, outputBuffers: FloatBuffer, inputFrames:Int) : Int
 }
